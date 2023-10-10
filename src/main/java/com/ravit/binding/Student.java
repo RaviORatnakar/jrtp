6d5b1705f793966fea7ff25490d485db7c4b1627
RaviORatnakar <raviratnakar71@gmail.com>
2023-10-10 16:07:11 +0530

    first commit

---
+++ src/main/java/com/ravit/binding/Student.java
@@ -0,0 +1,13 @@
+package com.ravit.binding;
+
+import lombok.Data;
+
+@Data
+public class Student {
+	private String name;
+	private String email;
+	private String gender;
+	private String course;
+	private String[] timing;
+
+}
---
+++ src/main/java/com/ravit/controller/StudentController.java
@@ -0,0 +1,8 @@
+package com.ravit.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StudentController {
+
+}
